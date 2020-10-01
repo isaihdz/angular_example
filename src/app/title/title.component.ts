32f3f73a6dd0de1920cb5bd7fc7f8649190e70c0
Isaí Hernández <iasiher13@gmail.com>
2020-09-30 20:15:21 -0500

    Example use of constructor and ngOninit to initialize values

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'title',
   templateUrl: 'title.component.html',
 })
-export class TitleComponent {
-  public name: string = 'Isaí ';
-  public edad: number = 27;
+export class TitleComponent implements OnInit {
+  public name: string;
+  public edad: number;
   public edades: number[];
+
+  /*constructor() {
+    this.name = 'Isaí';
+    this.edad = 27;}*/
+  constructor() {}
+
+  ngOnInit() {
+    this.name = 'Isaí';
+    this.edad = 27;
+  }
 
   getEdadNombre(): string {
     /**interpolation with string type js */
