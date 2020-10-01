39125edde431e0aa90589ba904ed0bef850213bd
Isaí Hernández <iasiher13@gmail.com>
2020-09-30 20:59:51 -0500

    ngContent example and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { FullScreenComponent } from './full-screen/full-screen.component';
 
 @NgModule({
-  declarations: [AppComponent, TitleComponent],
+  declarations: [AppComponent, TitleComponent, FullScreenComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
